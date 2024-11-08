7e68fb06fa69c28de726cb367ed9a08a5e47c0be
arthurmauliiengineering <arthurmauliiengineering@gmail.com>
2024-11-08 03:15:11 -0600

    refactor: refined world.py structure

--- arco/world.py
+++ arco/world.py
@@ -12,25 +12,29 @@ from arco.entity import Registry, is_relation, is_exclusive
 @datacls
 class World:
     registry: Registry = default(Registry)
+    entities_with: dict = default(dict)
     archetype_of: dict = default(dict)
     archetype_with_type: dict = default(dict)
     archetypes_with: dict = default(dict)
-    entities_with: dict = default(dict)
     alias_of: dict = default(dict)
 
     def __post_init__(self):
         self.origin = Archetype(self, tuple())
+        self.archetype_with_type[tuple()] = self.origin
         self.storable = self._new_entity()
         self.identity = self._init_storage(self._new_entity())
         self._set_identity(self.storable)
         self._set_identity(self.identity)
-        self.wildcard = self.spawn()
+        self.anything = self.spawn()
+
+    def __repr__(self):
+        return f"World({", ".join(str(archetype) for archetype in self.archetype_with_type.values())})"
 
     def _new_entity(self):
         entity = self.registry.spawn()
+        self.entities_with[entity] = list()
         self.archetype_of[entity] = self.origin
         self.archetypes_with[entity] = list()
-        self.entities_with[entity] = list()
         return entity
         
     def _set_identity(self, entity: int) -> int:
@@ -42,13 +46,13 @@ class World:
 
     def despawn(self, entity: int) -> None:
         for component in copy(self.archetype_of[entity].signature):
-            drop = self.unset if self.is_storable(component) else self.remove
-            drop(entity, component)
-        self.archetype_of.pop(entity)
-        self.archetypes_with.pop(entity)
+            self.unset(entity, component) if self.is_storable(component) else self.remove(entity, component)
         for attached in self.entities_with[entity]:
             self.remove(attached, entity)
         self.entities_with.pop(entity)
+        self.archetype_of.pop(entity)
+        self.archetypes_with.pop(entity)
+        self.registry.despawn(entity)
 
     def _init_storage(self, component: int) -> int:
         self.archetypes_with[component] = list()
@@ -63,12 +67,17 @@ class World:
         self.alias_of[cls.__name__] = self.component()
         return datacls(cls)
 
+    def release(self, component: int) -> None:
+        ...
+
     def add(self, entity: int, component: int) -> None:
         if component in self.archetype_of[entity].signature: return
+        self.entities_with[component].add(entity)
         self.archetype_of[entity].add(entity, component)
 
     def remove(self, entity: int, component: int) -> None:
         if component not in self.archetype_of[entity].signature: return
+        self.entities_with[component].remove(entity)
         self.archetype_of[entity].remove(entity, component)
 
     def set(self, entity: int, component: int, data: Any) -> None:
@@ -85,9 +94,6 @@ class World:
     def is_storable(self, component: int) -> bool:
         return self.storable in self.archetype_of[component].signature
 
-    def is_togglable(self, component: int) -> bool:
-        return self.togglable in self.archetype_of[component].signature
-
     def has(self, entity: int, component: int) -> bool:
         return component in self.archetype_of[entity].signature
 
@@ -101,12 +107,12 @@ class Archetype:
 
     def __post_init__(self):
         for component in self.signature:
-            self.link_component(component)
+            self.world.archetypes_with[component].append(self)
+            storable = self.world.is_storable(component)
+            if storable: self.storage_for[component] = dict()
 
-    def link_component(self, component: int) -> None:
-        self.world.archetypes_with[component].append(self)
-        storable = self.world.is_storable(component)
-        if storable: self.storage_for[component] = dict()
+    def __repr__(self):
+        return f"Archetype{self.signature}"
 
     def set(self, entity: int, component: int, data: Any) -> None:
         self.storage_for[component][entity] = data
