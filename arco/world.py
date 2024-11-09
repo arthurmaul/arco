747b93eccc84b1e6229ba9d3322d2dea31bb65c2
arthurmauliiengineering <arthurmauliiengineering@gmail.com>
2024-11-08 18:50:29 -0600

    refactor: began rework of world.py

--- arco/world.py
+++ arco/world.py
@@ -9,154 +9,128 @@ default = lambda constructor: field(default_factory=constructor)
 from arco.entity import Registry, is_relation, is_exclusive
 
 
+type EntityId = int
+type ComponentId = int
+type Signature = tuple[ComponentId, ...]
+type Column = dict[ComponentId, T]
+
+
 @datacls
 class World:
     registry: Registry = default(Registry)
-    entities_with: dict = default(dict)
-    archetype_of: dict = default(dict)
-    archetype_with_type: dict = default(dict)
-    archetypes_with: dict = default(dict)
-    alias_of: dict = default(dict)
+    entity_index: dict = default(dict) # archetype_of[eid]
+    archetype_index: dict = default(dict) # archetype_index[signature]
+    component_index: dict = default(dict) # archetypes_with[component]
+    alias_index: dict = default(dict) # alias_index[eid]
 
     def __post_init__(self):
-        self.origin = Archetype(self, tuple())
-        self.archetype_with_type[tuple()] = self.origin
-        self.storable = self._new_entity()
-        self.identity = self._init_storage(self._new_entity())
-        self._set_identity(self.storable)
-        self._set_identity(self.identity)
-        self.anything = self.spawn()
+        self.blank = Archetype(self, tuple())
+        self.archetype_with[tuple()] = self.origin
+        self.storable = self._assign_entity()
+        self.identity = self._assign_storage(self._assign_entity())
+        self._assign_identity(self.storable)
+        self._assign_identity(self.identity)
+        self.any = self.spawn()
 
     def __repr__(self):
-        return f"World({", ".join(str(archetype) for archetype in self.archetype_with_type.values())})"
+        return f"World({", ".join(str(archetype) for archetype in self.composed_of.values())})"
 
-    def _new_entity(self):
-        entity = self.registry.spawn()
-        self.entities_with[entity] = list()
-        self.archetype_of[entity] = self.origin
-        self.archetypes_with[entity] = list()
-        return entity
+    def _assign_eid(self):
+        eid = self.registry.create()
+        self.entities_with[eid] = list()
+        self.archetype_of[eid] = self.blank
+        self.archetypes_with[eid] = list()
+        return eid
         
-    def _set_identity(self, entity: int) -> int:
-        self.set(entity, self.identity, entity)
-        return entity
+    def _set_identity(self, eid: int) -> int:
+        self.set(eid, self.identity, eid)
+        return eid
 
     def spawn(self) -> int:
-        return self._set_identity(self._new_entity())
+        return self._set_identity(self._new_eid())
 
-    def despawn(self, entity: int) -> None:
-        for component in copy(self.archetype_of[entity].signature):
-            self.unset(entity, component) if self.is_storable(component) else self.remove(entity, component)
-        for attached in self.entities_with[entity]:
-            self.remove(attached, entity)
-        self.entities_with.pop(entity)
-        self.archetype_of.pop(entity)
-        self.archetypes_with.pop(entity)
-        self.registry.despawn(entity)
+    def despawn(self, eid: int) -> None:
+        for cid in copy(self.archetype_of[eid].signature):
+            self.unset(eid, cid) if self.is_storable(cid) else self.remove(eid, cid)
+        for owner in self.entities_with[eid]:
+            self.remove(owner, eid)
+        self.entities_with.pop(eid)
+        self.archetype_of.pop(eid)
+        self.archetypes_with.pop(eid)
+        self.registry.despawn(eid)
 
-    def _init_storage(self, component: int) -> int:
-        self.archetypes_with[component] = list()
-        self.add(component, self.storable)
-        return component
+    def _init_storage(self, cid: int) -> int:
+        self.archetypes_with[cid] = list()
+        self.add(cid, self.storable)
+        return cid
 
-    def component(self) -> int:
-        component = self.spawn()
-        return self._init_storage(component)
+    def cid(self) -> int:
+        cid = self.spawn()
+        return self._init_storage(cid)
 
     def datacls[T](self, cls: T) -> T:
-        self.alias_of[cls.__name__] = self.component()
+        self.alias_of[cls.__name__] = self.cid()
         return datacls(cls)
 
-    def release(self, component: int) -> None:
+    def release(self, cid: int) -> None:
         ...
 
-    def add(self, entity: int, component: int) -> None:
-        if component in self.archetype_of[entity].signature: return
-        self.entities_with[component].add(entity)
-        self.archetype_of[entity].add(entity, component)
+    def add(self, eid: int, cid: int) -> None:
+        if cid in self.archetype_of[eid].signature: return
+        self.entities_with[cid].add(eid)
+        self.archetype_of[eid].add(eid, cid)
 
-    def remove(self, entity: int, component: int) -> None:
-        if component not in self.archetype_of[entity].signature: return
-        self.entities_with[component].remove(entity)
-        self.archetype_of[entity].remove(entity, component)
+    def remove(self, eid: int, cid: int) -> None:
+        if cid not in self.archetype_of[eid].signature: return
+        self.entities_with[cid].remove(eid)
+        self.archetype_of[eid].remove(eid, cid)
 
-    def set(self, entity: int, component: int, data: Any) -> None:
-        self.add(entity, component)
-        self.archetype_of[entity].set(entity, component, data)
+    def set(self, eid: int, cid: int, data: Any) -> None:
+        self.add(eid, cid)
+        self.archetype_of[eid].set(eid, cid, data)
      
-    def unset(self, entity: int, component: int) -> None:
-        self.add(entity, component)
-        self.archetype_of[entity].unset(entity, component)
-
-    def get(self, entity: int, component: int) -> Any:
-        return self.archetype_of[entity].get(entity, component)
-
-    def is_storable(self, component: int) -> bool:
-        return self.storable in self.archetype_of[component].signature
-
-    def has(self, entity: int, component: int) -> bool:
-        return component in self.archetype_of[entity].signature
+    def unset(self, eid: int, cid: int) -> None:
+        self.add(eid, cid)
+        self.archetype_of[eid].unset(eid, cid)
 
 
 @datacls(slots=True)
 class Archetype:
     world: World
-    signature: tuple[int, ...]
-    storage_for: dict = default(dict)
+    signature: tuple[Component, ...]
+    storage_of: dict = default(dict)
     edges: dict = default(dict)
 
     def __post_init__(self):
-        for component in self.signature:
-            self.world.archetypes_with[component].append(self)
-            storable = self.world.is_storable(component)
-            if storable: self.storage_for[component] = dict()
+        for id in self.signature:
+            cid = self.world.owner_of[id]
+            if cid.is_storable():
+                self.storage_of[cid.id] = dict()
 
     def __repr__(self):
         return f"Archetype{self.signature}"
 
-    def set(self, entity: int, component: int, data: Any) -> None:
-        self.storage_for[component][entity] = data
+    def add(self, eid: int, cid: int) -> None:
+        if not self.edges.get(cid.id): 
+            signature = tuple(sorted((*self.signature, cid.id)))
+            archetype = self.world.archetype_with.setdefault(signature, Archetype(self.world, signature))
+            archetype.edges[cid.id] = self.edges[cid.id] = Edge(add=archetype, remove=self)
+        destination = self.edges[cid.id].add
+        for _cid in self.signature:
+            if self.world.eid_with[_cid].is_storable():
+                destination.storage_of[_cid][eid] = self.storage_of[_cid].pop(eid.id)
+        eid.archetype = destination
 
-    def unset(self, entity: int, component: int) -> Any:
-        return self.storage_for[component].pop(entity)
-
-    def add(self, entity: int, component: int) -> None:
-        if not self.edges.get(component): self.build_edge_adding(component)
-        destination = self.edges[component].add
-        for component in self.signature:
-            if not self.world.is_storable(component): continue
-            destination.set(entity, component, self.unset(entity, component))
-        self.world.archetype_of[entity] = destination
-
-    def remove(self, entity: int, component: int) -> None:
-        if not self.edges.get(component): self.build_edge_removing(component)
-        destination = self.edges[component].remove
-        for component in destination.signature:
-            if not self.world.is_storable(component): continue
-            destination.set(entity, component, self.unset(entity, component))
-        self.world.archetype_of[entity] = destination
-
-    def build_edge_adding(self, component: int):
-        signature = tuple(sorted((*self.signature, component)))
-        archetype = self.get_archetype_with(signature)
-        edge = Edge(add=archetype, remove=self)
-        archetype.edges[component] = self.edges[component] = edge
-
-    def build_edge_removing(self, component: int):
-        signature = tuple(key for key in self.signature if key != component)
-        archetype = self.get_archetype_with(signature)
-        edge = Edge(add=self, remove=archetype)
-        archetype.edges[component] = self.edges[component] = edge
-        return archetype
-
-    def get_archetype_with(self, signature: tuple[int, ...]) -> 'Archetype':
-        return self.world.archetype_with_type.setdefault(signature, Archetype(self.world, signature))
-
-    def get(self, entity: int, component: int) -> Any:
-        return self.storage_for[component][entity]
-
-    def has(self, component: int) -> bool:
-        return component in self.signature
+    def remove(self, eid: int, cid: int) -> None:
+        if not self.edges.get(cid.id):
+            signature = tuple(key for key in self.signature if not key is cid)
+            archetype = self.world.archetype_with_type.setdefault(signature, Archetype(self.world, signature))
+            archetype.edges[cid.id] = self.edges[cid.id] = Edge(add=self, remove=archetype)
+        destination = self.edges[cid.id].remove
+        for id in destination.signature:
+            if self.world.eid_with[id].is_storable():
+                destination.storage_of[id][eid.id] = self.storage_of[id].pop(eid.id)
+        eid.archetype = destination
 
 
 @datacls(slots=True)
